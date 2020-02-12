961ffdff0ba69695058134a9ab46c402c7c38e40
tnagai <nagai@zetacx.com>
2020-02-12 18:37:48 +0900

    add initializer to clean up

--- DenoisingAutoEncoder/main.py
+++ DenoisingAutoEncoder/main.py
@@ -1,4 +1,21 @@
+import os
+import glob
+
 from train import TrainDAE
+import settings
+
+class Initializer:
+
+    def cleanup(self):
+        self._clean_files('png')
+        self._clean_files('h5')
+
+    def _clean_files(self, kind='png'):
+        targets = glob.glob(os.path.join(settings.VAR_DIR, '**', f'*.{kind}'), recursive=True)
+        for t in targets:
+            if os.path.isfile(t):
+                os.remove(t)
 
 if __name__ == "__main__":
+    Initializer().cleanup()
     TrainDAE('masked').train()
