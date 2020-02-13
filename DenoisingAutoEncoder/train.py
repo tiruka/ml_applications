860b7eb24f0e368115554c83e7082cc9cbd3e914
tnagai <nagai@zetacx.com>
2020-02-13 10:32:13 +0900

    change name

--- DenoisingAutoEncoder/train.py
+++ DenoisingAutoEncoder/train.py
@@ -19,7 +19,8 @@ class TrainDAE:
         self._train(noised_x_train, x_train)
         noised_preds = self._predict(noised_x_test)
         for i in range(10):
-            self.save(i, x_test[i], noised_x_test[i], noised_preds[i])
+            np_img_list = [x_test[i], noised_x_test[i], noised_preds[i]]
+            self.save(i, np_img_list)
 
     def _train(self, X, Y):
         self.autoencoder.fit(
@@ -34,12 +35,12 @@ class TrainDAE:
     def _predict(self, noised_x_test):
         return self.autoencoder.predict(noised_x_test)
 
-    def save(self, num, x_test, noised_x_test, noised_preds):
-        comparable_img = self._get_concat_horizontal_multi_resize([x_test, noised_x_test, noised_preds])
+    def save(self, num, np_img_list):
+        comparable_img = self._get_concat_horizontal_multi_resize(np_img_list)
         comparable_img.save(os.path.join(settings.COMPARABLE_IMG, f'{num}_original.png'))
 
-    def _get_concat_horizontal_multi_resize(self, im_list, resample=Image.BICUBIC):
-        im_list = [array_to_img(x) for x in im_list]
+    def _get_concat_horizontal_multi_resize(self, np_img_list, resample=Image.BICUBIC):
+        im_list = [array_to_img(x) for x in np_img_list]
         min_height = min(im.height for im in im_list)
         im_list_resize = [im.resize((int(im.width * min_height / im.height), min_height), resample=resample)
                         for im in im_list]
