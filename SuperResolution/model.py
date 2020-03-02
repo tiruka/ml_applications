891c7e3759871fb0e1f4a9e0d76007b06dc5f8c4
tnagai <nagai@zetacx.com>
2020-03-02 19:17:01 +0900

    add super resolution with skip connections

--- SuperResolution/model.py
+++ SuperResolution/model.py
@@ -1,5 +1,5 @@
 import numpy as np
-from keras.layers import Conv2D
+from keras.layers import Conv2D, Input, Add, Conv2DTranspose
 from keras import backend as K
 from keras.models import (
     Model,
@@ -29,3 +29,32 @@ class SuperResolution(object):
         return -10 * K.log(
             K.mean(K.flatten((y_true - y_pred) ** 2))
             ) / np.log(10)
+
+
+class SuperResolutionWithSkipConnections(SuperResolution):
+    '''
+    Using Skip Connections for Super Resolution
+    '''
+    def build_model(self):
+        inputs = Input((None, None, 3), dtype='float')
+        # Encoder
+        conv1 = Conv2D(filters=64, kernel_size=3, padding='same')(inputs)
+        conv1 = Conv2D(filters=64, kernel_size=3, padding='same')(conv1)
+        conv2 = Conv2D(filters=64, kernel_size=3, strides=2, padding='same')(conv1)
+        conv2 = Conv2D(filters=64, kernel_size=3, padding='same')(conv2)
+        conv3 = Conv2D(filters=64, kernel_size=3, strides=2, padding='same')(conv2)
+        conv3 = Conv2D(filters=64, kernel_size=3, padding='same')(conv3)
+        # Decoder
+        deconv3 = Conv2DTranspose(filters=64, kernel_size=3, padding='same')(conv3)
+        deconv3 = Conv2DTranspose(filters=64, kernel_size=3, strides=2, padding='same')(deconv3)
+        merge_2 = Add()([deconv3, conv2]) # Skip Connections
+        deconv2 = Conv2DTranspose(filters=64, kernel_size=3, padding='same')(merge_2)
+        deconv2 = Conv2DTranspose(filters=64, kernel_size=3, strides=2, padding='same')(deconv2)
+        merge_1 = Add()([deconv2, conv1]) # Skip Connections
+        deconv1 = Conv2DTranspose(filters=64, kernel_size=3, padding='same')(merge_1)
+        deconv1 = Conv2DTranspose(filters=3, kernel_size=3, padding='same')(deconv1)
+        outputs = Add()([deconv1, inputs])
+        model = Model(inputs, outputs)
+        model.compile(optimizer='adam', loss=[self.psnr])
+        # model.summary()
+        return model
