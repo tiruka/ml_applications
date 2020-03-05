9c49a83412c8d294311f228dbfdd4b7e78156dab
tnagai <nagai@zetacx.com>
2020-03-05 18:39:39 +0900

    add style transfer base

--- StyleTransfer/sub_main.py
+++ StyleTransfer/sub_main.py
@@ -4,15 +4,16 @@ import math
 import random
 
 import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.python import keras
-from tensorflow.python.keras import backend as K
-from tensorflow.python.keras.models import Model, Sequential
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array, array_to_img
-from tensorflow.python.keras.layers import BatchNormalization, Conv2D, BatchNormalization, Add, Activation
+from keras import backend as K
+from keras.models import Model, Sequential
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array, array_to_img
+from keras.layers import Lambda, BatchNormalization, Conv2D, BatchNormalization, Add, Activation, Input, Conv2DTranspose
+from keras.applications import VGG16
+from keras.optimizers import Adadelta
 
 import settings
 
+input_shape = (224, 224, 3)
 
 
 def residual_block(input_tensor):
@@ -23,3 +24,158 @@ def residual_block(input_tensor):
     x = Conv2D(filters=128, kernel_size=(3, 3), strides=1, padding='same')(x)
     x = BatchNormalization()(x)
     return Add()([x, input_tensor])
+
+
+def build_encoder_decoder():
+    # Encoder
+    input_tensor = Input(shape=input_shape, name='input')
+    x = Lambda(lambda x: x / 255.)(input_tensor) # normalization [0, 1]
+    x = Conv2D(filters=32, kernel_size=(9, 9), strides=1, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=64, kernel_size=(3, 3), strides=2, padding='same')(x)
+    x = Conv2D(filters=128, kernel_size=(3, 3), strides=2, padding='same')(x)
+    for _ in range(5): # Add five residual blocks
+        x = residual_block(x)
+    # Decoder
+    x = Conv2DTranspose(filters=64, kernel_size=(3, 3), strides=2, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2DTranspose(filters=32, kernel_size=(3, 3), strides=2, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2DTranspose(filters=3, kernel_size=(3, 3), strides=2, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('tahn')(x)
+    outputs = Lambda(lambda x: (x + 1) * 127.5) # transform outputs to [0, 255]
+    model_gen = Model(inputs=input_tensor, outputs=outputs)
+    return model_gen
+
+model_gen = build_encoder_decoder()
+
+vgg16 = VGG16()
+for layer in vgg16.layers:
+    layyer.trainable = False
+
+def norm_vgg16(x):
+    '''
+    RGB -> BGR transformation and approximate centerlization
+    '''
+    return (x[:, :, :, ::-1] - 120) / 255.
+
+style_layer_names = frozenset(['block1_conv2', 'block2_conv2', 'block3_conv3', 'block4_conv3'])
+contents_layer_names = frozenset(['block3_conv3'])
+style_outputs_gen = []
+contents_outputs_gen = []
+
+input_gen = model_gen.output # inputs will come from stlyle-transform model
+z = Lambda(norm_vgg16)(input_gen)
+for layer in vgg16.layers: # Recontructing network by piling up 
+    z = layer(z)
+    if layer.name in style_layer_names:
+        style_outputs_gen.append(z)
+    if layer.name in contents_layer_names:
+        contents_outputs_gen.append(z)
+
+model = Model(inputs=model_gen.input, outputs=style_outputs_gen + contents_outputs_gen)
+
+
+img_arr_style = np.expand_dims(array_to_img(load_img(path, target_size=input_shape[:2])), axis=0)
+style_outputs = []
+input_style = Input(shape=input_shape, name='input_style')
+x = Lambda(norm_vgg16)(input_style)
+for layer in vgg16.layers:
+    x = layer(x)
+    if layer.name in style_layer_names:
+        style_outputs.append(x)
+
+model_style = Model(inputs=input_style, outputs=style_outputs)
+y_true_style = model_style.predict(img_arr_style)
+
+
+input_contents = Input(shape=input_shape, name='input_contents')
+contents_outputs = []
+y = Lambda(norm_vgg16)(input_contents)
+for layer in vgg16.layers:
+    y = layer(y)
+    if layer.name in contents_layer_names:
+        contents_outputs.append(y)
+model_contents = Model(inputs=input_contents, outputs=contents_outputs)
+
+
+
+def load_images(image_path_list, target_size):
+    '''
+    Return batch of array from image_path_list
+    '''
+    _load_img = lambda x: img_to_array(load_img(x, target_size=target_size))
+    image_list = [np.expand_dims(_load_img(path), axis=0) for path in image_path_list]
+    return np.concatenate(image_list, axis=0)
+
+def train_generator(image_path_list, batch_size, model, y_true_style, shuffle=True, epochs=10):
+    '''
+    Generate train data
+    '''
+    n_samples = len(image_path_list)
+    indices = [i for i in range(n_samples)]
+    steps_per_epochs = math.ceil(n_samples / batch_size)
+    image_path_ndarray = np.array(image_path_list)
+    count_epochs = 0
+    while True:
+        count_epochs += 1
+        if shuffle:
+            np.random.shuffle(indices)
+        for i in range(steps_per_epochs):
+            start = batch_size * i
+            end = batch_size * (i + 1)
+            X = load_images(image_path_ndarray[indices[start:end]])
+            batch_size_act = X.shape[0]
+            y_true_style_t = [np.repeat(feat, batch_size_act, axis=0) for feat in y_true_style]
+            y_true_contents = model.predict(X)
+            yield X, y_true_style_t + [y_true_contents]
+
+
+
+gen = train_generator(
+    image_path_list,
+    batch_size,
+    model_contents,
+    y_true_style,
+    epochs=10,
+)
+
+
+
+def feature_loss(y_true, y_pred):
+    '''
+    Loss function of contents features
+    '''
+    norm = K.prod(K.cast(K.shape(y_true)[1:], 'float32'))
+    return K.sum(K.square(y_pred - y_true), axis=(1, 2, 3)) / norm
+
+
+def gram_matrix(x):
+    '''
+    Measure approximation of contents by gram matrix which calculates dot of feature maps
+    '''
+    x_sw = K.permute_dimensions(x, (0, 3, 2, 1)) # axis change
+    s = K.shape(x_sw)
+    new_shape = (s[0], s[1], s[2] * s[3])
+    x_rs = K.reshape(x_sw, new_shape)
+    x_rs_t = K.permute_dimensions(x_rs, (0, 2, 1)) # transpose
+    dot = K.batch_dot(x_rs, x_rs_t) # calculates dot
+    norm = K.prod(K.cast(s[1:], 'float32'))
+    return dot / norm
+
+
+def style_loss(y_true, y_pred):
+    '''
+    Loss function of style features
+    '''
+    return K.sum(K.square(gram_matrix(y_pred) - gram_matrix(y_true)), axis=(1, 2))
+
+
+model.compile(optimizer=Adadelta(),
+              loss=[style_loss, style_loss, style_loss, style_loss, feature_loss],
+              loss_weights=[1.0, 1.0, 1.0, 1.0, 3.0])
+
